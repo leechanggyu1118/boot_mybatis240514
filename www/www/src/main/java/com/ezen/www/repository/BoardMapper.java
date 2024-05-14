9d48cbc8476ac49a9d35dd558c6b650163cfdc96
leechanggyu <leechanggyu1118@naver.com>
2024-05-14 23:29:11 +0900

    file upload

--- www/www/src/main/java/com/ezen/www/repository/BoardMapper.java
+++ www/www/src/main/java/com/ezen/www/repository/BoardMapper.java
@@ -1,6 +1,7 @@
 package com.ezen.www.repository;
 
 import com.ezen.www.domain.BoardVO;
+import com.ezen.www.domain.PagingVO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -10,11 +11,15 @@ public interface BoardMapper {
 
     int insert(BoardVO bvo);
 
-    List<BoardVO>  list();
+    List<BoardVO>  list(PagingVO pgvo);
 
     BoardVO getDetail(long bno);
 
     int modify(BoardVO bvo);
 
     void remove(long bno);
+
+    int getTotalCount(PagingVO pgvo);
+
+    long getBno();
 }
--- www/www/src/main/java/com/ezen/www/service/BoardService.java
+++ www/www/src/main/java/com/ezen/www/service/BoardService.java
@@ -1,17 +1,21 @@
 package com.ezen.www.service;
 
+import com.ezen.www.domain.BoardDTO;
 import com.ezen.www.domain.BoardVO;
+import com.ezen.www.domain.PagingVO;
 
 import java.util.List;
 
 public interface BoardService {
-    int register(BoardVO bvo);
+    int register(BoardDTO bdto);
 
-    List<BoardVO> list();
+    List<BoardVO> list(PagingVO pgvo);
 
-    BoardVO getDetail(long bno);
+    BoardDTO getDetail(long bno);
 
     int modify(BoardVO bvo);
 
     void remove(long bno);
+
+    int getTotalCount(PagingVO pgvo);
 }
